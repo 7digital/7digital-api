eafb880da1a3e1203c3069d9f5df1a1d6472fce4
Raoul Millais <raoul@raoulmillais.com>
2011-06-30 15:21:37 +0100

    Added documentation comments to config

--- config.js
+++ config.js
@@ -1,6 +1,28 @@
 exports.Config = {
+	/**
+	* Path to the json file defining the remote api actions and their mappings
+	* onto the wrapper. 
+	*
+	* @type string
+	*/
 	schemapath: 'lib/api.json',
+	/**
+	* Your API consumer key for accessing oauth secured endpoints
+	* see http://api.7digital.com/1.2/static/documentation/7digitalpublicapi.html#Introduction
+	*
+	* @type string
+	*/
 	oauthkey: 'YOUR_KEY_HERE',
+	/**
+	* Your oauth consumer secret for signing oauth secured request urls.
+	*
+	* @type string
+	*/
 	oauthsecret: '',
+	/**
+	* Enables verbose logging to the console of all api requests and responses when set to true
+	*
+	* @type boolean
+	*/
 	debug: true
 };
