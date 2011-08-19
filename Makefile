d8de7bdd1361bd0d34596620147b8645c3d0250c
Raoul Millais <raoul@raoulmillais.com>
2011-08-19 16:46:31 +0100

    Changed oauth docs to docco style and added step in makefile to generate docs

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ SHELL = bash
 test: check
 	jessie spec/
 
-check:
+check: docs
 	readyjs readyjs.conf.js
+
+docs:
+	docco lib/oauth-helper.js
+	docco examples/oauth.js
+
+.PHONY: test check docs
 
--- examples/oauth.js
+++ examples/oauth.js
@@ -1,25 +1,25 @@
+// 
+// Module dependencies
+// 
 var
-	/**
-	 * Module dependencies
-	 */
 	Step = require('step'),
 	readline = require('readline'),
 	oauthHelper = require('../lib/oauth-helper'),
-	/**
-	 * Create a readline interface for prompting the user
-	 */
+	// 
+	// Create a readline interface for prompting the user
+	// 
 	consoleInterface = readline.createInterface(process.stdin, process.stdout),
-	/*
-	 * Consumer key and secret
-	 */
+	// 
+	// Consumer key and secret
+	// 
 	consumerkey = 'YOUR_KEY_HERE',
 	consumersecret = 'YOUR_SECRET_HERE';
 
 Step(
 	function getRequestToken() {
-		/**
-		 * Get a request token using the oauth helper
-		 */
+		// 
+		// Get a request token using the oauth helper
+		// 
 		oauthHelper.getRequestToken({
 			oauthkey: consumerkey,
 			oauthsecret: consumersecret,
@@ -27,37 +27,37 @@ Step(
 			}, this);
 	},
 	function authorise(err, requestToken, requestSecret, authoriseUrl) {
-		/**
-		 * Throw the error if there is one
-		 */
+		// 
+		// Throw the error if there is one
+		// 
 		if (err) {
 			throw new Error(err);
 		}
 
-		/**
-		 * Show the authorise url
-		 */
+		// 
+		// Show the authorise url
+		// 
 		console.log('Authorise here: %s', authoriseUrl);
 
-		/**
-		 * Remember the token and secret so we can access it after the
-		 * user presses enter
-		 */
+		// 
+		// Remember the token and secret so we can access it after the
+		// user presses enter
+		// 
 		 this.requestToken = requestToken;
 		 this.requestSecret = requestSecret;
 
-		/**
-		 * Tell the user to visit the authorise url
-		 */
+		// 
+		// Tell the user to visit the authorise url
+		// 
 		consoleInterface.question('Visit the link to authorise this application to' +
 									' access your 7digital account.  Press enter to' +
 									' continue', this);
 	},
 	function continueAfterAuthorisation() {
-		/**
-		 * Get an access token using the oauth helper using the authorised
-		 * request token and secret
-		 */
+		// 
+		// Get an access token using the oauth helper using the authorised
+		// request token and secret
+		// 
 		oauthHelper.getAccessToken({
 				oauthkey: consumerkey,
 				oauthsecret: consumersecret,
@@ -66,30 +66,30 @@ Step(
 			}, this);
 	},
 	function logTheAccessToken(err, accesstoken, accesssecret) {
-		/**
-		 * Log any error
-		 */
+		// 
+		// Log any error
+		// 
 		if (err) {
 			console.log(err);
-			/**
-			 * Close the readline interface properly so that the process
-			 * ends cleanly otherwise it will hang.
-			 */
+			// 
+			// Close the readline interface properly so that the process
+			// ends cleanly otherwise it will hang.
+			// 
 			consoleInterface.close();
 			process.stdin.destroy();
 			return;
 		}
 
-		/**
-		 * Write the token and secret out to the commandline
-		 */
+		// 
+		// Write the token and secret out to the commandline
+		// 
 		console.log('Access Token: %s', accesstoken);
 		console.log('Access Secret: %s', accesssecret);
 
-		/**
-		 * Close the readline interface properly so that the process
-		 * ends cleanly otherwise it will hang.
-		 */
+		// 
+		// Close the readline interface properly so that the process
+		// ends cleanly otherwise it will hang.
+		// 
 		consoleInterface.close();
 		process.stdin.destroy();
 	}
