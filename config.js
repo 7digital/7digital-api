5782840315004e0bf9812114ea1afbe284531852
Raoul Millais <raoul@raoulmillais.com>
2011-08-21 15:55:48 +0100

    Fixing jslint violations and whitespace

--- config.js
+++ config.js
@@ -12,23 +12,23 @@ exports.Config = {
 	// @type string
 	consumerkey: 'YOUR_KEY_HERE',
 	// Your oauth consumer secret for signing oauth secured request urls.
-	// 
+	//
 	// @type string
 	consumersecret: 'YOUR_SECRET_HERE',
 	// Enables verbose logging to the console of all api requests and responses
 	// when set to true.
-	// 
+	//
 	// @type boolean
 	debug: true,
 	// The desired format of API responses
 	// Sets format to JSON by default
-	// 
+	//
 	// @type string
 	format: 'json',
 	// A logger object for outputting messages. This should be an obect, which
 	// has function properties for each of the npm loglevels. Winston provides
 	// a default implementation.
-	// 
+	//
 	// @type {Winston.Logger} or any object which defines methods for each of
 	// the npm loglevels.
 	logger: require('./lib/logger')
