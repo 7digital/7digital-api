a956028f05fcb1efd54a958c1e911f0e9deff942
Raoul Millais <raoul@raoulmillais.com>
2011-08-22 13:35:09 +0100

    Collapsed request contructor arguments into an options hash. Stopped parsing XML when the requested format is XML

--- examples/simpleclient.js
+++ examples/simpleclient.js
@@ -1,6 +1,10 @@
 var api = require('../index'),
 	artists = new api.Artists();
 
-artists.getReleases({ artistid: 1 }, function (err, data) {
+artists.getReleases({ artistid: 1 }, function processReleases(err, data) {
+	if (err) {
+		throw new Error(err);
+	}
+
 	console.dir(data);
 });
