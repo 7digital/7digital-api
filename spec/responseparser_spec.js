03247f825a714c92f4029142858970f3dda99d6b
Raoul Millais <raoul@raoulmillais.com>
2011-09-14 08:59:18 +0100

    Add example responses and test for XML response parsing

---
+++ spec/responseparser_spec.js
@@ -0,0 +1,23 @@
+var ResponseParser = require('../lib/responseparser'),
+	winston = require('winston'),
+	fs = require('fs'),
+	path = require('path');
+
+describe('responseparser', function() {
+	var parser = new ResponseParser({
+		format: 'xml',
+		logger: new winston.Logger({ transports: [] })
+	});
+
+	it('should return xml when format is xml', function() {
+		var callbackSpy = jasmine.createSpy(),
+			xml = fs.readFileSync(
+				path.join(__dirname +
+					'/responses/release-tracks-singletrack.xml'),
+					'utf8');
+		parser.parse(callbackSpy, null, xml);
+		expect(callbackSpy).toHaveBeenCalled();
+		expect(callbackSpy).toHaveBeenCalledWith(null, xml);
+	});
+
+});
