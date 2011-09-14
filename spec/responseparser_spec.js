91ea8bacfefc231cc18c472bdd11c50e14370cf8
Raoul Millais <raoul@raoulmillais.com>
2011-09-14 09:47:58 +0100

    Add case sensitive test for format in response parser

--- spec/responseparser_spec.js
+++ spec/responseparser_spec.js
@@ -20,4 +20,16 @@ describe('responseparser', function() {
 		expect(callbackSpy).toHaveBeenCalledWith(null, xml);
 	});
 
+	it('should return xml when format is XML', function() {
+		var callbackSpy = jasmine.createSpy(),
+			xml = fs.readFileSync(
+				path.join(__dirname +
+					'/responses/release-tracks-singletrack.xml'),
+					'utf8');
+
+		parser.format = 'XML';
+		parser.parse(callbackSpy, null, xml);
+		expect(callbackSpy).toHaveBeenCalled();
+		expect(callbackSpy).toHaveBeenCalledWith(null, xml);
+	});
 });
