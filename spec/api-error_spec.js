8a67a5af6ab7a13a50c55a61714dcd43e6fa4093
Raoul Millais <raoul@raoulmillais.com>
2013-11-14 16:28:23 +0000

    Error messages for Api Errors

---
+++ spec/api-error_spec.js
@@ -0,0 +1,26 @@
+var ApiError = require('../lib/apierror'),
+	winston = require('winston');
+
+require('./custom-matchers.js');
+
+describe('API Error', function() {
+
+	it('should use the response body as the message', function() {
+		var msg = 'You have exceeded your daily request limit of 4000';
+		var err = new ApiError(401, { body: msg });
+
+		expect(err.message).toEqual(msg);
+	});
+
+	it('should use the status code as the message when the body is empty', function() {
+		var err = new ApiError(401, { body: '' });
+
+		expect(err.message).toEqual('Unexpected 401 status code');
+	});
+
+	it('should use the status code as the message when the response is not defined', function() {
+		var err = new ApiError(401);
+
+		expect(err.message).toEqual('Unexpected 401 status code');
+	});
+});
