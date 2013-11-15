bc7e3813553754365b1c64b88b89ea9e58e72f7d
Raoul Millais <raoul@raoulmillais.com>
2013-11-15 15:31:51 +0000

    Api error gets resonse body not response

--- spec/api-error_spec.js
+++ spec/api-error_spec.js
@@ -7,13 +7,13 @@ describe('API Error', function() {
 
 	it('should use the response body as the message', function() {
 		var msg = 'You have exceeded your daily request limit of 4000';
-		var err = new ApiError(401, { body: msg });
+		var err = new ApiError(401, msg);
 
 		expect(err.message).toEqual(msg);
 	});
 
 	it('should use the status code as the message when the body is empty', function() {
-		var err = new ApiError(401, { body: '' });
+		var err = new ApiError(401, '');
 
 		expect(err.message).toEqual('Unexpected 401 status code');
 	});
