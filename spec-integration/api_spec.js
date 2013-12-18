9e16ffa98825027d5b290f887f18c5f9e1e4de8f
Dave Aitken <dave.aitken@gmail.com>
2013-12-18 10:49:27 +0000

    Move api stub setup into relevant tests file

--- spec-integration/api_spec.js
+++ spec-integration/api_spec.js
@@ -19,6 +19,37 @@ var api = new Api({
 var expect = require('chai').expect;
 
 describe('api', function () {
+	var childProcess = require('child_process');
+	var serverProcess;
+	var processStarted;
+
+	before(function (done) {
+		var nodeProcessPath;
+		try {
+			nodeProcessPath = require.resolve('api-stub');
+			console.log('spawning node api-stub process : ', nodeProcessPath, '\n');
+
+			serverProcess = childProcess.spawn('node', [nodeProcessPath], {});
+
+			serverProcess.stdout.on('data', function (data) {
+				if (!processStarted) {
+					processStarted = true;
+					done();
+				}
+			});
+
+			serverProcess.stderr.on('data', function (data) {
+				console.log('' + data);
+			});
+		} catch (e) {
+			console.log('Make sure api-stub module is installed!');
+		}
+	});
+
+	after(function () {
+		console.log('\nshutting down api-stub process');
+		serverProcess.kill('SIGKILL');
+	});
 
 	it('should get a release by id', function (done) {
 		var releases = new api.Releases();
