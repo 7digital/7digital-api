1da5825f48befbde5ed5d82216f12f2b669c8618
Raoul Millais <raoul@raoulmillais.com>
2014-02-04 15:51:09 +0000

    Make request static

---
+++ spec/request_spec.js
@@ -0,0 +1,32 @@
+'use strict';
+var expect = require('chai').expect;
+var request = require('../lib/request');
+
+describe('request', function () {
+
+	describe('createHeaders', function () {
+
+		it('adds the user adent and host headers', function () {
+			var headers  = request.createHeaders('api.7digital.com');
+			expect(headers.host).to.equal('api.7digital.com');
+			expect(headers['User-Agent']).to.equal('Node.js HTTP Client');
+		});
+
+	});
+
+	describe('prepare', function () {
+		it('adds the consumer key to the params', function () {
+			var preparedData = request.prepare({}, 'YOUR_KEY_HERE');
+			expect(preparedData.oauth_consumer_key).to.equal('YOUR_KEY_HERE');
+		});
+
+		it('converts dates to the correct format', function () {
+			var preparedData = request.prepare({
+				someParam: new Date(2005, 5, 3)
+			});
+
+			expect(preparedData.someParam).to.equal('20050603');
+		});
+	});
+
+});
