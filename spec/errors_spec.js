e803f6798ba3c2b9999168328223d3d59b19f321
Dave Aitken <dave.aitken@gmail.com>
2014-04-24 14:42:36 +0100

    Rename APIError to HttpError

--- spec/errors_spec.js
+++ spec/errors_spec.js
@@ -1,23 +1,23 @@
 var expect = require('chai').expect;
-var ApiError = require('../lib/apierror');
+var ApiHttpError = require('../lib/errors').ApiHttpError;
 
-describe('API Error', function() {
+describe('API HTTP Error', function() {
 
 	it('should use the response body as the message', function() {
 		var msg = 'You have exceeded your daily request limit of 4000';
-		var err = new ApiError(401, msg);
+		var err = new ApiHttpError(401, msg);
 
 		expect(err.message).to.equal(msg);
 	});
 
 	it('should use the status code as the message when the body is empty', function() {
-		var err = new ApiError(401, '');
+		var err = new ApiHttpError(401, '');
 
 		expect(err.message).to.equal('Unexpected 401 status code');
 	});
 
 	it('should use the status code as the message when the response is not defined', function() {
-		var err = new ApiError(401);
+		var err = new ApiHttpError(401);
 
 		expect(err.message).to.equal('Unexpected 401 status code');
 	});
