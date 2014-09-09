ac49f6f06e4ca48316449c67a726df56de92e497
Raoul Millais <raoul@raoulmillais.com>
2014-09-09 13:00:08 +0100

    Rename and move array normalisation
    - Start moving response santisation out of responseparser into new
    cleaners file with normaliseResourceArrays -> ensureColections.
    - Test cleaners separately and allow passing of collectionPaths
    - Rename tests to better describe behaviour

---
+++ test/cleaners-test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var cleaners = require('../lib/cleaners');
+var assert = require('chai').assert;
+
+describe('cleaners', function () {
+
+	function createOptsWithFormat(format) {
+		return {
+			format: format,
+			logger: { silly: function () {} }
+		};
+	}
+
+	describe('ensureCollections', function () {
+
+		it('ensures collections one level deep', function() {
+			var response = require(
+				'./responses/parsed/release-tracks-singletrack.json');
+			var cleaned = cleaners.ensureCollections(
+				[ 'tracks.track' ], response);
+
+			assert.instanceOf(cleaned.tracks.track, Array);
+		});
+
+		it('ensures collections two levels deep', function () {
+			var response = require(
+				'./responses/parsed/release-single-format.json');
+
+			var cleaned = cleaners.ensureCollections(
+				[ 'release.formats.format' ], response);
+			assert.instanceOf(response.release.formats.format, Array);
+		});
+
+		it('ensures nested collections', function () {
+			var lockerTrackArray;
+			var response = require(
+				'./responses/parsed/locker-single-release-and-track.json');
+
+			var cleaned = cleaners.ensureCollections(
+				[
+					'locker.lockerReleases.lockerRelease',
+					'locker.lockerReleases.lockerRelease.lockerTracks.lockerTrack'
+				], response);
+			var lockerReleaseArray =
+				response.locker.lockerReleases.lockerRelease;
+			assert.instanceOf(lockerReleaseArray, Array);
+			lockerTrackArray = lockerReleaseArray[0].lockerTracks.lockerTrack;
+			assert.instanceOf(lockerTrackArray, Array);
+		});
+
+		it('preserves collections when they are already arrays', function () {
+			var response = require('./responses/parsed/list-multiple.json');
+
+			var cleaned = cleaners.ensureCollections(
+				[ 'list.listItems.listItem' ], response);
+			assert.instanceOf(response.list.listItems.listItem, Array);
+		});
+
+		// Note that basket items are one level deeper than other arrays, hence
+		// the separate test.
+		it('normalises basket items into an array', function () {
+			var response = require('./responses/parsed/basket-additem.json');
+
+			var cleaned = cleaners.ensureCollections(
+				[ 'basket.basketItems' ], response);
+			assert.instanceOf(response.basket.basketItems, Array);
+		});
+
+		it('handles the empty basket case', function () {
+			var response = require('./responses/parsed/basket-empty.json');
+			var cleaned = cleaners.ensureCollections(
+				[ 'basket.basketItems' ], response);
+			assert.instanceOf(response.basket.basketItems, Array);
+			assert.lengthOf(response.basket.basketItems, 0);
+		});
+
+	});
+});
